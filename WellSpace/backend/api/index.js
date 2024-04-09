0d4e3c80b7b80ed4f9dac750d076799220f0cd1b
a1847791 <a1847791@adelaide.edu.au>
2024-04-09 20:33:38 +0930

    Add login and register routes.
    - Also, add the auth routes to the index.js

--- WellSpace/backend/api/index.js
+++ WellSpace/backend/api/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./Routes/auth.js";
 
 dotenv.config()
 
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
+app.use('/api/v1/auth', authRoute)
 
 mongoose.set('strictQuery', false)
 
