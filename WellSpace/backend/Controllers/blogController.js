bea9591ebbc6cd0eb55f54d413b53cc16e0f067c
a1847791 <a1847791@adelaide.edu.au>
2024-04-22 22:10:55 +0930

    Add Blog API routes and controllers.

---
+++ WellSpace/backend/Controllers/blogController.js
@@ -0,0 +1,89 @@
+import Blog from "../Models/BlogSchema.js"
+import Doctor from "../Models/DoctorSchema.js"
+
+export const createBlog = async (req, res) => {
+  const id = req.params.doctorId
+  const { title, content } = req.body
+
+  try {
+    const newBlog = new Blog({
+      title,
+      content,
+      doctor: id
+    })
+
+    const savedBlog = await newBlog.save()
+
+    await Doctor.findByIdAndUpdate(id, {
+      $push: {
+        blogs: savedBlog._id
+      }
+    })
+
+    res.status(200).json({
+      success: true,
+      message: "Blog Submitted",
+      data: savedBlog
+    })
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+}
+
+export const getAllBlog = async (req, res) => {
+  try {
+
+    const { query } = req.query
+    let blogs
+
+    if (query) {
+      blogs = await Blog.find({
+        isPublished: 'published',
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { content: { $regex: query, $options: "i" } }
+        ]
+      })
+    } else {
+      blogs = await Blog.find({ isPublished: 'published' })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: 'All Blogs found',
+      data: blogs
+    })
+
+  } catch (error) {
+    console.log(error)
+    res.status(404).json({
+      success: false,
+      message: 'No Blogs found'
+    })
+  }
+}
+
+export const getSingleBlog = async (req, res) => {
+  const id = req.params.blogId
+
+  try {
+
+    const singleBlog = await Blog.findById(id).populate('doctor')
+
+    res.status(200).json({
+      success: true,
+      message: 'Blog successfully found',
+      data: singleBlog
+    })
+
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: 'No Blog found'
+    })
+  }
+}
--- WellSpace/backend/api/index.js
+++ WellSpace/backend/api/index.js
@@ -6,7 +6,8 @@ import dotenv from "dotenv";
 import authRoute from "../Routes/auth.js";
 import doctorRoute from "../Routes/doctor.js";
 import userRoute from "../Routes/user.js";
-import feedbackRoute from "../Routes/feedback.js"
+import feedbackRoute from "../Routes/feedback.js";
+import blogRoute from "../Routes/blog.js";
 // import Doctor from "../Models/DoctorSchema.js"
 // import { doctors } from "../seed/doctors.js"
 // import User from "../Models/UserSchema.js"
@@ -32,6 +33,7 @@ app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/doctors', doctorRoute)
 app.use('/api/v1/users', userRoute)
 app.use('/api/v1/feedbacks', feedbackRoute)
+app.use('/api/v1/blogs', blogRoute)
 
 mongoose.set('strictQuery', false)
 
