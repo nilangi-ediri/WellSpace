b2e7ae34f5c0deb5aa74f8647a2c91ba723ad265
a1847791 <a1847791@adelaide.edu.au>
2024-05-24 21:59:42 +0930

    Add reply APIs
    - Add createReply, getReplyByCommentId, deleteReply.
    - Update comment schema.
    - Update seed data for both comment and reply.

---
+++ WellSpace/backend/seed/reply.js
@@ -0,0 +1,26 @@
+export const replies = [
+  {
+    _id: "67044187b5fda9ee74326509",
+    comment: "67035797b5fda9ee74326519",
+    user: "6602d3d1155e8f469cffdde9",
+    replyText: "You're right ❤️",
+  },
+  {
+    _id: "67044187b5fda9ee74326510",
+    comment: "67044187b5fda9ee74326519",
+    user: "6602d3d1155e8f469cffdde8",
+    replyText: "Agree ✨",
+  },
+  {
+    _id: "67044187b5fda9ee74326511",
+    comment: "66035787b5fda9ee74324512",
+    doctor: "660247adb56a95c2c97fa68c",
+    replyText: "That's right 💀",
+  },
+  {
+    _id: "67044187b5fda9ee74326512",
+    comment: "66024fb2d9dd3754be293fdc",
+    doctor: "660247adb56a95c2c97fa68b",
+    replyText: "Yeah, I felt the same 💀",
+  },
+]
