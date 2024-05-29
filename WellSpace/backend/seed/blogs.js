ec53aa68be5402ca070b36f3de2d25b8eb1510d7
a1847791 <a1847791@adelaide.edu.au>
2024-05-29 17:35:59 +0930

    Capitalize the category name's first letter to align with frontend case-sensitive tag filter.
    - For the blog list page.

--- WellSpace/backend/seed/blogs.js
+++ WellSpace/backend/seed/blogs.js
@@ -7,7 +7,7 @@ export const blogs = [
     content: "Anxiety disorders are more than just feeling nervous or worried; they are persistent and overwhelming feelings of fear and apprehension that can interfere with daily life. Learn about different types of anxiety disorders such as generalized anxiety disorder (GAD), panic disorder, and social anxiety disorder, along with effective coping strategies and treatment options to manage symptoms.",
     isPublished: "published",
     summary: "Understand anxiety disorders: GAD, panic, social anxiety. Coping strategies, treatment.",
-    category: "anxiety",
+    category: "Anxiety",
     comments: [
       "67035797b5fda9ee74326519"
     ],
@@ -20,7 +20,7 @@ export const blogs = [
     content: "Depression affects millions of people worldwide, yet stigma often prevents individuals from seeking help. Let's challenge the stigma surrounding depression by fostering open conversations and promoting understanding. Learn how to recognize the signs of depression, reach out for support, and access resources for treatment and recovery.",
     isPublished: "published",
     summary: "Combat depression stigma: open conversations, understanding. Recognize signs, seek support, treatment.",
-    category: "depression",
+    category: "Depression",
     comments: [
       "67044187b5fda9ee74326519"
     ],
@@ -33,7 +33,7 @@ export const blogs = [
     content: "Stress is a natural response to life's challenges, but excessive stress can take a toll on our mental and physical well-being. Discover practical techniques for managing stress, including mindfulness meditation, relaxation exercises, and time management strategies. Empower yourself to build resilience and thrive in the face of adversity.",
     isPublished: "pending",
     summary: "Manage stress: mindfulness, relaxation, time management. Build resilience, thrive.",
-    category: "stress",
+    category: "Stress",
     comments: [
       "66035797b5fda9ee74326517"
     ],
@@ -46,7 +46,7 @@ export const blogs = [
     content: "Losing a loved one is one of life's most challenging experiences, and grief can manifest in various ways. Explore the grieving process and learn healthy ways to cope with loss, such as expressing emotions, seeking support from others, and honoring the memory of your loved one. Find solace in knowing that healing is a journey, and it's okay to seek help along the way.",
     isPublished: "published",
     summary: "Navigate grief: express, seek support, honor memories. Healing journey, seek help.",
-    category: "grief",
+    category: "Grief",
     comments: [
       "66035787b5fda9ee74324412"
     ],
@@ -59,7 +59,7 @@ export const blogs = [
     content: "Self-care is essential for maintaining mental and emotional well-being, yet it's often overlooked in our busy lives. Discover the importance of self-care practices such as regular exercise, adequate sleep, healthy nutrition, and setting boundaries. Prioritize self-care as a valuable investment in your overall health and happiness.",
     isPublished: "published",
     summary: "Prioritize self-care: exercise, sleep, nutrition, boundaries. Invest in well-being.",
-    category: "self-care",
+    category: "Self-Care",
     comments: [
       "66035787b5fda9ee74324512"
     ],
@@ -72,7 +72,7 @@ export const blogs = [
     content: "Trauma can have a profound impact on individuals, affecting their relationships, emotions, and sense of safety. Learn about the different types of trauma, including childhood trauma, PTSD, and complex trauma, and explore evidence-based approaches to healing, such as therapy, EMDR, and trauma-informed care. Remember that healing is possible, and you deserve support on your journey to recovery.",
     isPublished: "pending",
     summary: "Understanding relationships, emotions, safety. Explore healing approaches for recovery.",
-    category: "trauma",
+    category: "Therapy",
     comments: [
       "66035787b5fdb9ee74324412"
     ],
