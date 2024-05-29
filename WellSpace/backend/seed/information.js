f5e334d7948e1ff2f108d8df2e479887ce29b30c
Siyu Ni <siyu.ni@student.adelaide.edu.au>
2024-05-30 00:57:31 +0930

    Update information.js

--- WellSpace/backend/seed/information.js
+++ WellSpace/backend/seed/information.js
@@ -1,25 +1,4 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import Information from '../Models/InformationSchema.js'; // Adjust the path as necessary
-
-dotenv.config();
-
-const seedInformation = [
+export const seedInformation = [
   { title: "About Us", content: "This is the about us page content." },
   { title: "Contact", content: "Contact us at info@example.com." }
 ];
-
-const insertInformationEntries = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    await Information.deleteMany(); // Warning: This will delete all existing entries!
-    await Information.insertMany(seedInformation);
-    console.log('Information entries have been seeded');
-  } catch (error) {
-    console.error('An error occurred while seeding information entries:', error);
-  } finally {
-    mongoose.disconnect();
-  }
-};
-
-insertInformationEntries();
